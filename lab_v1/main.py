ab5be615eb27bcc69774ac16e2901580f19a959c
unknown <1724381733@qq.com>
2021-11-27 21:31:29 +0800

    example

--- lab_v1/main.py
+++ lab_v1/main.py
@@ -1,16 +1,41 @@
+import numpy as np
+from scipy import optimize
+import matplotlib.pyplot as plt
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], dtype=float)
+y = np.array([5, 7, 9, 11, 13, 15, 28.92, 42.81, 56.7, 70.59,
+              84.47, 98.36, 102.25, 106.14, 110.03])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+# 一个输入序列，4个未知参数，2个分段函数
+def piecewise_linear(x, x0, y0, k1, k2):
+    # x<x0 ⇒ lambda x: k1*x + y0 - k1*x0
+    # x>=x0 ⇒ lambda x: k2*x + y0 - k2*x0
+    return np.piecewise(x, [x < x0, x >= x0], [lambda x: k1 * x + y0 - k1 * x0,
+                                               lambda x: k2 * x + y0 - k2 * x0])
+
+
+def piecewise_linear3(x, x0, x1, y0, y1, k0, k1):
+    return np.piecewise(x, [x <= x0, np.logical_and(x0 < x, x <= x1), x > x1],
+                        [lambda x: k0 * (x - x0) + y0,  # 根据点斜式构建函数
+                         lambda x: (x - x0) * (y1 - y0) / (x1 - x0) + y0,  # 根据两点式构建函数
+                         lambda x: k1 * (x - x1) + y1])
+
+
+# 用已有的 (x, y) 去拟合 piecewise_linear 分段函数
+p, e = optimize.curve_fit(piecewise_linear3, x, y, bounds=(0, [6, 13, 20, 120, 4, 6]))
+
+xd = np.linspace(0, 15, 200)
+plt.plot(x, y, "o")
+plt.plot(xd, piecewise_linear3(xd, *p))
+plt.show()
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
